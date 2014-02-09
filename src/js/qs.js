a3f3bc8af4520ebd1a1b69880b95a3f20263d313
customcommander <hello@spinjs.com>
2014-02-09 23:20:19 +0000

    add a get method to the object returned by QS()

--- src/js/qs.js
+++ src/js/qs.js
@@ -1,3 +1,17 @@
+function split_pairs(qstr, psep, vsep) {
+    return qstr.split(psep).map(function (pair) {
+        pair = pair.split(vsep);
+        return { key: pair[0], value: pair[1] };
+    });
+}
+
+function merge_pairs_into_object(pairs) {
+    var obj =  {};
+    pairs.forEach(function (pair) {
+        obj[pair.key] = pair.value;
+    });
+    return obj;
+}
 
 /**
  * @param {String} [qstr] The query string. Default to the current query string.
@@ -5,6 +19,25 @@
  * @param {String} [vsep] Value separator. Default to '='.
  */
 function QS(qstr, psep, vsep) {
+
+    var qry_obj = {};
+
+    qstr = typeof qstr === 'undefined' ? document.location.search.substr(1) : qstr;
+    psep = typeof psep === 'undefined' ? '&' : psep;
+    vsep = typeof vsep === 'undefined' ? '=' : vsep;
+
+    if (typeof qstr === 'string' && qstr) {
+        qry_obj = merge_pairs_into_object(split_pairs(qstr, psep, vsep));
+    }
+
+    return {
+        get: function (key, def_val) {
+            if (qry_obj[key] === undefined) {
+                return def_val;
+            }
+            return decodeURIComponent(qry_obj[key]);
+        }
+    }
 }
 
 NS.QS = QS;
--- src/test/unit/js/qs-tests.js
+++ src/test/unit/js/qs-tests.js
@@ -8,6 +8,38 @@ suite.add(new Y.Test.Case({
 
     "is a global function": function () {
         Y.Assert.isFunction(window.QS);
+    },
+
+    "returns an object": function () {
+        Y.Assert.isObject(window.QS());
+    }
+}));
+
+suite.add(new Y.Test.Case({
+
+    name: "QS() - get()",
+
+    "returns the value of given key": function () {
+        var qry = QS('foo=bar');
+        Y.Assert.areSame('bar', qry.get('foo'));
+    },
+
+    "returns the decoded value of given key": function () {
+        var qry = QS('foo='+encodeURIComponent('http://foo.url?a=1'));
+        Y.Assert.areSame('http://foo.url?a=1', qry.get('foo'));
+    },
+
+    "returns undefined if given key is not found": function () {
+        var qry = QS('foo=bar');
+        Y.Assert.areSame(undefined, qry.get('xyz'));
+    },
+
+    "returns the default value if given key is not found": function () {
+        var qry = QS('foo=bar');
+        Y.Assert.areSame(123, qry.get('xyz', 123));
+        Y.Assert.areSame(0, qry.get('xyz', 0));
+        Y.Assert.areSame(false, qry.get('xyz', false));
+        Y.Assert.areSame(null, qry.get('xyz', null));
     }
 }));
 
