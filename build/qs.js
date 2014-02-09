2c8d1321f73ae9c149d37a4820a9fc3850f6ffcc
customcommander <hello@spinjs.com>
2014-02-09 23:20:50 +0000

    build files

--- build/qs.js
+++ build/qs.js
@@ -1,5 +1,19 @@
 (function (NS) {
 
+function split_pairs(qstr, psep, vsep) {
+    return qstr.split(psep).map(function (pair) {
+        pair = pair.split(vsep);
+        return { key: pair[0], value: pair[1] };
+    });
+}
+
+function merge_pairs_into_object(pairs) {
+    var obj =  {};
+    pairs.forEach(function (pair) {
+        obj[pair.key] = pair.value;
+    });
+    return obj;
+}
 
 /**
  * @param {String} [qstr] The query string. Default to the current query string.
@@ -8,6 +22,24 @@
  */
 function QS(qstr, psep, vsep) {
 
+    var qry_obj = {};
+
+    qstr = typeof qstr === 'undefined' ? document.location.search.substr(1) : qstr;
+    psep = typeof psep === 'undefined' ? '&' : psep;
+    vsep = typeof vsep === 'undefined' ? '=' : vsep;
+
+    if (typeof qstr === 'string' && qstr) {
+        qry_obj = merge_pairs_into_object(split_pairs(qstr, psep, vsep));
+    }
+
+    return {
+        get: function (key, def_val) {
+            if (qry_obj[key] === undefined) {
+                return def_val;
+            }
+            return decodeURIComponent(qry_obj[key]);
+        }
+    }
 }
 
 NS.QS = QS;
