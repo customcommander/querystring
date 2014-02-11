f07f78854ec0728898ed4c55f60f55d28bf9754d
customcommander <hello@spinjs.com>
2014-02-11 09:37:29 +0000

    add yuidoc blocks

--- src/js/qs.js
+++ src/js/qs.js
@@ -14,6 +14,19 @@ function merge_pairs_into_object(pairs) {
 }
 
 /**
+ * Parse or build a query string.
+ *
+ * @example
+ *      // By default, QueryString will use the document url
+ *      // e.g. http://foo.bar?hello=word
+ *      var qry = QS();
+ *
+ * @example
+ *      // Or you can supply your own query string
+ *      var qry = QS('a=1&b=2');
+ *
+ * @class QS
+ * @constructor
  * @param {String} [qstr] The query string. Default to the current query string.
  * @param {String} [psep] Parameter separator. Default to '&'.
  * @param {String} [vsep] Value separator. Default to '='.
@@ -31,6 +44,25 @@ function QS(qstr, psep, vsep) {
     }
 
     return {
+
+        /**
+         * Returns the value of given parameter.
+         *
+         * @example
+         *      // http://foo.bar?hello=world
+         *      var qry = QS();
+         *      qry.get('hello'); // world
+         *
+         * @example
+         *      var qry = QS('a=1');
+         *      qry.get('a'); // 1
+         *
+         * @method get
+         * @for QS
+         * @param {String} key Name of the parameter to get.
+         * @param {Any} [def_val] Default value to return should the parameter not be found.
+         * @return {Any} The parameter value or the default value (`def_val`) or `undefined`.
+         */
         get: function (key, def_val) {
             if (qry_obj[key] === undefined) {
                 return def_val;
